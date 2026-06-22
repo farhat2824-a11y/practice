4d78afe11a1ed4e9410c11f161187f53ee287cc5
farhat2824-ally <farhat2824@gmail.com>
2026-06-22 17:41:45 +0530

    Added new files

---
+++ project123/src/test/java/tests/SampleTest11.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest11 {
+
+	@Test
+	void test() {
+		System.out.println("Test7");
+	}
+
+}
